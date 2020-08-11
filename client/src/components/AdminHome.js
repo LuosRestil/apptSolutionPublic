7e5be424c89b3e31bf8c96cb37553d79a8770069
LuosRestil <metaltasteslikechicken@gmail.com>
2020-08-11 14:56:40 -0500

    Added todo notes, updated store name on landing page

--- client/src/components/AdminHome.js
+++ client/src/components/AdminHome.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const AdminHome = props => {
   return (
     <div>
-      <h1 className="mt-5">Welcome to McKay's By Appointment!</h1>
+      <h1 className="mt-5">Welcome to McCoy's By Appointment!</h1>
       <h3 className="mt-5 mb-5">Would you like to...</h3>
       <ul className="user-home-list">
         <li className="user-home-list-item">
